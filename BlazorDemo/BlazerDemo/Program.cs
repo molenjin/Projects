ade4baee07ba2427702492693820662c78d558f5
molenjin <39934180+molenjin@users.noreply.github.com>
2025-05-17 21:25:15 +0800

    Add files via upload

---
+++ BlazorDemo/BlazerDemo/Program.cs
@@ -0,0 +1,27 @@
+using BlazorDemo.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddBlazorBootstrap();
+builder.Services.AddSingleton<AirplaneService>();
+builder.Services.AddSingleton<D3PieChartRecords>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();

968eb816cfb3c1f8bd8df990475de324712875c3
molenjin <39934180+molenjin@users.noreply.github.com>
2025-06-01 18:45:19 +0800

    Add files via upload

---
+++ BlazorForum/BlazorForum/Libraries/LibFormat.cs
@@ -0,0 +1,39 @@
+﻿using System.Web;
+
+namespace BlazorForum.Libraries
+{
+    public class LibFormat
+    {
+        public static string HtmlDecode(string text)
+        {
+            return HttpUtility.HtmlDecode(text);
+        }
+
+        public static string TitleLink(string title)
+        {
+            string titleLink = title.ToLower().Trim();
+
+            titleLink = System.Text.RegularExpressions.Regex.Replace(titleLink, @"[^0-9a-zA-Z ]+", "");
+            titleLink = System.Text.RegularExpressions.Regex.Replace(titleLink, "/-+/", " ");
+            titleLink = System.Text.RegularExpressions.Regex.Replace(titleLink, "\\s+", "-");
+            titleLink = System.Text.RegularExpressions.Regex.Replace(titleLink, "/-+/", "-");
+
+            return titleLink;
+        }
+
+        public static string NumberOfCommentsText(int numOfComments)
+        {
+            return numOfComments == 0 ? " " : $"({numOfComments})";
+        }
+
+        public static string NumberOfCommentsLink(int numOfComments)
+        {
+            return numOfComments == 0 ? " " : $"/{numOfComments}";
+        }
+
+        public static string HiddenText(bool hidden)
+        {
+            return hidden ? $"(hidden) " : " ";
+        }
+    }
+}

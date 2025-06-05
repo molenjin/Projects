605d7b8bde83897e3f63dc3f22db8ca7769dbc96
molenjin <molenjin@gmail.com>
2025-06-06 00:31:42 +0800

    LibValidate improved

--- BlazorForum/BlazorForum/Libraries/LibValidate.cs
+++ BlazorForum/BlazorForum/Libraries/LibValidate.cs
@@ -6,81 +6,85 @@ namespace BlazorForum.Libraries
 {
     public class LibValidate(IDatabaseCalls DatabaseCalls)
     {
-        public async Task<string> ValidateCommentAsync(Comment comment)
+        public async Task<(bool, string)> IsCommentValidAsync(Comment comment)
         {
-            //------ Banned IP            
-            if (await DatabaseCalls.IsBannedIpAsync(comment.IP))
-                return "You are not allowed to post on this forum";          
+            //------ If comment is null
+            if (comment == null)
+                throw new ArgumentNullException("Comment cannot be null");
+
+            //------ If not new topic and Title length > 0
+            if (!IsNewTopic(comment.TopicId) && !string.IsNullOrWhiteSpace(comment.Title))
+                throw new Exception("Title not allowed for comments");
 
             //------ If not new topic and TopicExists(TopicID) = FALSE
             if ((!IsNewTopic(comment.TopicId)) && !(await DatabaseCalls.TopicExistsAsync(comment.TopicId)))
-                return "SYSTEM ERROR: TopicID not found among existing topics";
+                throw new Exception("TopicID not found among existing topics");
+
+            //------ Banned IP            
+            if (await DatabaseCalls.IsBannedIpAsync(comment.IP))
+                return (false, "You are not allowed to post on this forum");
 
             //------ If Name length = 0
-            if (string.IsNullOrEmpty(comment.Name.Trim()))
-                return "User name is missing";
+            if (string.IsNullOrWhiteSpace(comment.Name))
+                return (false, "User name is missing");
 
             //------ If new topic and Title length = 0
-            if (IsNewTopic(comment.TopicId) && string.IsNullOrEmpty(comment.Title.Trim()))
-                return "Topic title is missing";
-
-            //------ If not new topic and Title length > 0
-            if (!IsNewTopic(comment.TopicId) && !string.IsNullOrEmpty(comment.Title.Trim()))
-                return "SYSTEM ERROR: Title not allowed for comments";
+            if (IsNewTopic(comment.TopicId) && string.IsNullOrWhiteSpace(comment.Title))
+                return (false, "Topic title is missing");
 
             //------ If Text length = 0
-            if (string.IsNullOrEmpty(comment.Text.Trim()))
-                return "Comment text is missing";
+            if (string.IsNullOrWhiteSpace(comment.Text))
+                return (false, "Comment text is missing");
 
             //------ UserName min length (2)
             if (comment.Name.Length < 2)
-                return "User name is too short (min. 2 characters required)";
+                return (false, "User name is too short (min. 2 characters required)");
 
             //------ UserName max length (20)
             if (comment.Name.Length > 20)
-                return "User name is too long (max. 20 characters allowed)";
+                return (false, "User name is too long (max. 20 characters allowed)");
 
             //------ Title min length (2)
             if ((IsNewTopic(comment.TopicId) && comment.Title.Length < 2))
-                return "Topic title is too short (min. 2 characters required)";
+                return (false, "Topic title is too short (min. 2 characters required)");
 
             //------ Title max length (60)
             if (comment.Title.Length > 60)
-                return "Topic title is too long (max. 60 characters allowed)";
+                return (false, "Topic title is too long (max. 60 characters allowed)");
 
             //------ Text min length (2)
             if (comment.Text.Length < 2)
-                return "Comment text is too short (min. 2 characters allowed)";
+                return (false, "Comment text is too short (min. 2 characters allowed)");
 
             //------ Text max length (5000)
             if (comment.Text.Length > 5000)
-                return "Comment text is too long (max. 5000 characters allowed)";
+                return (false, "Comment text is too long (max. 5000 characters allowed)");
 
             //------ Invalid characters in UserName
             if (!IsValidString(comment.Name))
-                return "User name contains invalid characters";
+                return (false, "User name contains invalid characters");
 
             //------ Invalid characters in Title
             if (!IsValidString(comment.Title))
-                return "Topic title contains invalid characters";
+                return (false, "Topic title contains invalid characters");
 
             //------ Invalid characters in Text
             if (!IsValidString(comment.Text))
-                return "Comment text contains invalid characters";
+                return (false, "Comment text contains invalid characters");
 
             //------ Improper content in UserName
             if (IsImproperContent(comment.Name))
-                return "User name contains improper content";
+                return (false, "User name contains improper content");
 
             //------ Improper content in Title
             if (IsImproperContent(comment.Title))
-                return "Topic title contains improper content";
+                return (false, "Topic title contains improper content");
 
             //------ Improper content in Text
             if (IsImproperContent(comment.Text))
-                 return "Comment text contains improper content";
+                 return (false, "Comment text contains improper content");
 
-            return string.Empty;
+            return (true, string.Empty);
         }
 
         private static bool IsValidString(string content)
